043fe794946e360a82551a09b27f91acbc706a37
Daniel-Mazzilli <danmazzilli@pursuit.org>
2024-03-08 08:59:44 -0500

    initial commit

---
+++ readme.md
@@ -0,0 +1,21 @@
+#SQL code warmup
+
+This is a warmup exercise for the class to review the basics of SQL
+
+##Getting started
+
+- clone this repository
+- cd into to directory
+- ls make sure you see the file videogames.sql
+- psql
+- \i videogames.sql to run the file videogames.sql
+
+##instructions
+
+You will write your code between the `--` lines for each echo that is given.
+
+To test your code, each time, run `\i videogames.sql` in the psql part of the terminal either in VSCode or in your iTerm. Not both. Make sure your Postgres app is running.
+
+Remember, each time that you run the above command you are recreating the database and the tables and re-running all the queries.
+
+If you feel your terminal is too crowded when testing, feel free to comment out the previous problems.
---
+++ videogames.sql
@@ -0,0 +1,54 @@
+-- do not update this code
+DROP DATABASE IF EXISTS videogames;
+DROP TABLE IF EXISTS game_franchises;
+-- add your code below
+
+\echo - Create a database videogames
+--
+
+--
+
+\echo - Connect to the videogames db
+--
+
+--
+
+\echo - Create a table for game_franchises
+-- with the following columns
+-- id SERIAL PRIMARY KEY
+-- name VARCHAR(30) NOT NULL
+-- company (VARCHAR 30)
+-- year INT can only be greater than 1950
+--
+
+--
+
+\echo - Alter the table add a column for have_played with a BOOLEAN type
+--
+
+--
+
+\echo - Insert three entries into your game_franchises table. Create your own data
+--
+
+--
+
+\echo - See the data in your game_franchises table
+--
+
+--
+
+\echo - See only the name and have_played column of your game_franchises table
+--
+
+--
+
+\echo — Select all rows from game_franchises and order by name alphabetically
+--
+
+--
+
+\echo — Select all rows from game_franchises, order by year descending
+--
+
+--
